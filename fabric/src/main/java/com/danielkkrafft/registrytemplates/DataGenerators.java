72310458b341c6af9c2e0a5fa1fb543f3fe254cb
Daniel Krafft <daniel@krafftmedia.co>
2026-04-21 20:51:03 -0600

    fabric datagen fix

--- fabric/src/main/java/com/danielkkrafft/registrytemplates/DataGenerators.java
+++ fabric/src/main/java/com/danielkkrafft/registrytemplates/DataGenerators.java
@@ -1,11 +1,15 @@
 package com.danielkkrafft.registrytemplates;
 
+import com.danielkkrafft.registrytemplates.template.RTDataProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class DataGenerators implements DataGeneratorEntrypoint {
     @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        DataGeneratorsInternal.initData(fabricDataGenerator);
+    public void onInitializeDataGenerator(FabricDataGenerator generator) { // api data is only generated on neoforge side to avoid headaches
+        FabricDataGenerator.Pack pack = generator.createPack();
+        for (RTDataProvider<?> provider : RegistryTemplates.INSTANCES.get(generator.getModId()).getAll(RTDataProvider.class)) {
+            pack.addProvider((o, r) -> provider.supplier.apply(o, r, provider.owner));
+        }
     }
 }
